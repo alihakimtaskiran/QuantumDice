ceec22903e585d5e88c3656e82d239158ba7ea6e
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2021-07-19 18:44:27 +0300

    Add files via upload

---
+++ quantumdice.py
@@ -0,0 +1,20 @@
+from qiskit import Aer, QuantumCircuit, execute
+qd=QuantumCircuit(3,3)
+init_st=[1/2**.5,1/2**.5]
+for i in range(3):
+    qd.initialize(init_st,i)
+    qd.measure(i,i)
+qc=Aer.get_backend("statevector_simulator")
+def roll_dice():
+    r=tuple(execute(qd,qc).result().get_counts())[0][::-1]
+    val=0
+    for i in range(3):
+        val+=int(r[i])*2**i
+    if val not in (1,2,3,4,5,6):
+        val=roll_dice()
+    return val
+while True:
+	input("Press enter to roll Quantum-Dice")
+	print(roll_dice())
+
+
