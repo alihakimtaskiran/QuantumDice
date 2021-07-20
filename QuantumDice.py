2175bb1c9e8a4c8066002b79f7bc208cc1b907cb
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2021-07-20 16:36:43 +0300

    Update QuantumDice.py

--- QuantumDice.py
+++ QuantumDice.py
@@ -1,8 +1,8 @@
 from qiskit import Aer, QuantumCircuit, execute
 qd=QuantumCircuit(3,3)
 for i in range(3):
-    qd.h(i)
-    qd.measure(i,i)
+    qd.h(0)
+    qd.measure(0,i)
 qc=Aer.get_backend("statevector_simulator")
 def roll_dice():
     r=tuple(execute(qd,qc).result().get_counts())[0][::-1]
@@ -13,5 +13,5 @@ def roll_dice():
         val=roll_dice()
     return val
 while True:
-        input("Press enter to roll Quantum-Dice")
-        print(roll_dice())
+	input("Press enter to roll Quantum-Dice")
+	print(roll_dice())
